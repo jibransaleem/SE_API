d326fe66bf22887280b3ce531595cebfb4710d1c
JibranSaleem <156086813+jibransaleem@users.noreply.github.com>
2025-11-11 10:55:47 +0500

    Refactor database connection setup
    Updated database connection details and improved security by encoding the password.

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from urllib.parse import quote_plus
+from urllib.parse import quote_plus
 
-password = "MKGFt3llxU"  # 
-DATABASE_URL = f"mysql+pymysql://sql12805698:{password}@sql12.freesqldatabase.com:3306/sql12805698"
+# Your original password with special characters
+password = ""
+db_name =""
+# Encode it for URL use
+encoded_password = quote_plus(password)  
 
-engine = create_engine(DATABASE_URL, echo=True)
+# Correct connection URL
+DATABASE_URL = f"mysql+pymysql://root:{encoded_password}@127.0.0.1:3306/{db_name}"
+engine = create_engine(DATABASE_URL, echo=True,pool_pre_ping=True,pool_recycle=1800   )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
